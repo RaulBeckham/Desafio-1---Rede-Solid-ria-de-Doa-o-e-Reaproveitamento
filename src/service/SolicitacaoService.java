d8869ba2e8116ec8debfe92e11d0f66998704e61
RaulBeckham <raulbeckhambarrossoares@gmail.com>
2026-05-21 22:35:57 +0000

    commit 9

---
+++ src/service/SolicitacaoService.java
@@ -0,0 +1,54 @@
+package service;
+
+import model.ItemDoacao;
+import model.Solicitacao;
+import repository.SolicitacaoRepository;
+
+public class SolicitacaoService {
+
+    private SolicitacaoRepository solicitacaoRepository;
+
+    public SolicitacaoService(SolicitacaoRepository solicitacaoRepository) {
+        this.solicitacaoRepository = solicitacaoRepository;
+    }
+
+    public boolean validarQuantidade(ItemDoacao item, int quantidadeSolicitada) {
+
+    if (quantidadeSolicitada > item.getQuantidade()) {
+        return false;
+    }
+
+    return true;
+   } 
+
+   public void criarSolicitacao(Solicitacao solicitacao) {
+
+    boolean quantidadeValida = validarQuantidade(
+            solicitacao.getItem(),
+            solicitacao.getQuantidadeSolicitada());
+
+   if (quantidadeValida) {
+
+    solicitacao.setStatus("APROVADA");
+
+    ItemDoacao item = solicitacao.getItem();
+
+    int novaQuantidade =
+            item.getQuantidade() - solicitacao.getQuantidadeSolicitada();
+
+    item.setQuantidade(novaQuantidade);
+
+    if (item.getQuantidade() == 0) {
+    item.setStatus("ENTREGUE");
+    } else {
+    item.setStatus("DISPONIVEL");
+    }
+
+    solicitacaoRepository.adicionarSolicitacao(solicitacao);
+
+    System.out.println("Solicitação aprovada!");
+
+}
+}
+
+}
