aaa051b5a682965bb03c3c2bf6a4ac2f4f6870c9
RaulBeckham <raulbeckhambarrossoares@gmail.com>
2026-05-21 22:33:39 +0000

    commit 7

---
+++ src/util/MenuUtil.java
@@ -0,0 +1,17 @@
+package util;
+
+public class MenuUtil {
+
+    public static void exibirMenu() {
+
+        System.out.println("\n=== MENU ===");
+        System.out.println("1 - Cadastrar Doador");
+        System.out.println("2 - Cadastrar Beneficiario");
+        System.out.println("3 - Cadastrar Item");
+        System.out.println("4 - Listar Doadores");
+        System.out.println("5 - Listar Beneficiarios");
+        System.out.println("6 - Listar Itens");
+        System.out.println("7 - Solicitar Item");
+        System.out.println("0 - Sair");
+    }
+}
